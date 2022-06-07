1b115eec138c6b25d111807e59c87a020a4e60cb
Lorena Cueva <lorenacueva@gmail.com>
2022-06-07 15:53:49 -0700

    commit tree 2a89efa212d084e3000e89fc0bf3109f2f163d4e

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,11 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const p = projects.map(project => <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}></ProjectItem>);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{p}</div>
     </div>
   );
 }
